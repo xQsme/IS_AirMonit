5c4b13dde2ba6346e16261c67a02a7f290770136
Gonçalo Graça Ribeiro <2151159@my.ipleiria.pt>
2017-11-13 12:02:08 +0000

    Atualização do DLog
    Dlog agr funciona por message broker

--- AirMonit/AirMonit/AirMonit DLog/Models/Entry.cs
+++ AirMonit/AirMonit/AirMonit DLog/Models/Entry.cs
@@ -1,29 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace AirMonit_DLog.Models
 {
     public class Entry
     {
-        public int id { get; set; }
-        public Parameter parameter { get; set; }
-        public decimal value { get; set; }
+        public int no2 { get; set; }
+        public int co { get; set; }
+        public int o3 { get; set; }
         public DateTime date { get; set; }
         public City city { get; set; }
 
-        public enum Parameter
-        {
-            NITROGEN_DIOXIDE,
-            CARBON_MONOXIDE,
-            OZONE
-        }
         public enum City
         {
             LEIRIA,
-            LISBOA,
-            PORTO
+            COIMBRA,
+            PORTO,
+            LISBOA
         }
     }
 }
