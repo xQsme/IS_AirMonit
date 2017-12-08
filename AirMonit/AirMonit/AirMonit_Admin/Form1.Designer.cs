8eca9f5e1810f29d3be570ee0c6554b7656b11bd
Gonçalo Graça Ribeiro <2151159@my.ipleiria.pt>
2017-12-08 19:53:53 +0000

    AirMonitAdmin implementado

---
+++ AirMonit/AirMonit/AirMonit_Admin/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace AirMonit_Admin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePickerInicio = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerFim = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listViewAlarme = new System.Windows.Forms.ListView();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(295, 81);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(689, 357);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(346, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aplicação de Gestão";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cidade:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(16, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(260, 28);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Todas as cidades";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Data de Início:";
+            // 
+            // dateTimePickerInicio
+            // 
+            this.dateTimePickerInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dateTimePickerInicio.Location = new System.Drawing.Point(16, 197);
+            this.dateTimePickerInicio.Name = "dateTimePickerInicio";
+            this.dateTimePickerInicio.Size = new System.Drawing.Size(260, 26);
+            this.dateTimePickerInicio.TabIndex = 5;
+            this.dateTimePickerInicio.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePickerFim
+            // 
+            this.dateTimePickerFim.CustomFormat = "";
+            this.dateTimePickerFim.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dateTimePickerFim.Location = new System.Drawing.Point(16, 283);
+            this.dateTimePickerFim.Name = "dateTimePickerFim";
+            this.dateTimePickerFim.Size = new System.Drawing.Size(260, 26);
+            this.dateTimePickerFim.TabIndex = 7;
+            this.dateTimePickerFim.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Data de Fim:";
+            // 
+            // listViewAlarme
+            // 
+            this.listViewAlarme.Location = new System.Drawing.Point(16, 466);
+            this.listViewAlarme.Name = "listViewAlarme";
+            this.listViewAlarme.Size = new System.Drawing.Size(975, 267);
+            this.listViewAlarme.TabIndex = 8;
+            this.listViewAlarme.UseCompatibleStateImageBehavior = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(16, 443);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Alarmes:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 745);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listViewAlarme);
+            this.Controls.Add(this.dateTimePickerFim);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePickerInicio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "AirMonit Admin";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePickerInicio;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFim;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listViewAlarme;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ AirMonit/AirMonit/AirMonit_Admin/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AirMonit_Admin
+{
+    public partial class Form1 : Form
+    {
+
+        private static string URL = "http://localhost:50077/";
+        private static string CITIES_ENDPOINT = "api/cities";
+
+        public Form1()
+        {
+            InitializeComponent();
+            dateTimePickerInicio.Value = DateTime.Today.AddDays(-7);
+            dateTimePickerFim.Value = DateTime.Today;
+
+            dateTimePickerInicio.MaxDate = dateTimePickerFim.Value;
+            dateTimePickerFim.MinDate = dateTimePickerInicio.Value;
+
+            string cities_uri = URL + CITIES_ENDPOINT;
+
+            /*
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(cities_uri);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            response.GetResponseStream();
+            */
+
+            listViewAlarme.Columns.Add("Data");
+            listViewAlarme.Columns.Add("Cidade");
+            listViewAlarme.Columns.Add("Partícula");
+            listViewAlarme.Columns.Add("Valor");
+            listViewAlarme.Columns.Add("Mensagem");
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePickerFim.MinDate = dateTimePickerInicio.Value;
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePickerInicio.MaxDate = dateTimePickerFim.Value;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
