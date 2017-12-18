70b0577960395c2f36854d9c87be4d101ab955f9
Gonçalo Graça Ribeiro <2151159@my.ipleiria.pt>
2017-12-18 11:42:15 +0000

    trigger da tabelas ENTRIES adicionado

--- DB/CREATE tables/dbo.ENTRIES.sql
+++ DB/CREATE tables/dbo.ENTRIES.sql
@@ -20,4 +20,53 @@ CREATE TABLE [dbo].[ENTRIES] (
     [cityId] INT            NOT NULL
 );
 
-
+CREATE TRIGGER AddNewParticleToCityAverage
+    ON [dbo].[ENTRIES]
+    AFTER INSERT
+    AS
+    DECLARE @week int;
+    DECLARE @month int;
+    DECLARE @year int;
+    DECLARE @particle nchar(5);
+    DECLARE @value numeric(6,2);
+    DECLARE @cityId int;
+    SELECT @week = DATEDIFF(WEEK, DATEADD(MONTH, DATEDIFF(MONTH, 0, ins.date), 0), ins.date) +1, 
+            @month = MONTH(ins.date), 
+            @year = YEAR(ins.date), 
+            @particle = ins.name,
+            @value = ins.value,
+            @cityId = ins.cityId
+    FROM inserted ins;
+    IF NOT EXISTS (SELECT particle
+                  FROM CityAverage ca
+                  WHERE @year = ca.Year AND
+                    @month = ca.Month AND
+                    @week = ca.Week AND
+                    ca.cityId = @cityId AND
+                    @particle = ca.particle)
+    BEGIN
+        INSERT INTO CityAverage (cityId, particle, Year, MONTH, WEEK, count, sum)
+            VALUES (@cityId, @particle, @year, @month, @week, 1, @value)
+    END
+    ELSE
+    BEGIN
+        UPDATE CityAverage
+        SET SUM = (SUM + @value),
+            COUNT = (COUNT + 1),
+            average = ((SUM + @value) / (COUNT + 1)),
+            min = (CASE WHEN min > @value
+                    THEN @value
+                    ELSE min
+                    END),
+            max = (CASE WHEN max < @value
+                    THEN @value
+                    ELSE max
+                    END)
+        WHERE Id = (SELECT ca.Id 
+                            FROM CityAverage ca
+                            WHERE @year = ca.Year AND
+                                  @month = ca.Month AND
+                                  @week = ca.Week AND
+                                  ca.cityId = @cityId AND
+                                  @particle = ca.particle)
+    END;
